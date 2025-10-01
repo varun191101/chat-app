6a82e9435b7c5bc0ba280f25b1a7cea6b32bd63f
Varun Yadav <varunyadav191101@gmail.com>
2025-10-01 09:52:02 +0530

    Server Got Updated

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import http from 'http';
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
-import { Server } from "socket.io"; 
+import { Server } from "socket.io";
 
 // create express app and http server
 const app = express();
@@ -36,7 +36,7 @@ io.on("connection", (socket) => {
 
 
 // middleware setup
-app.use(express.json({limit: "4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("Server is live"));
@@ -46,5 +46,10 @@ app.use("/api/messages", messageRouter)
 // connect to MongoDB
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT))
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log("Server is running on PORT: " + PORT))
+}
+
+// export server for vercel
+export default index;
